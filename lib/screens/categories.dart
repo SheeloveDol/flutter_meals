cfbbf9dd8bd2a67e4ba4678db5cfedeb33e3806d
Sheelove Dol <siesel26@gmail.com>
2024-04-03 01:01:10 -0700

    Added category parameter to _selectCategory navigation function

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
@@ -7,12 +8,17 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   // Defining function to navigate to the meals screen
-  void _selectCategory(BuildContext context) {
+  // This function receives the context and the category
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(
-          title: 'Cat from route',
-          meals: [],
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -38,8 +44,8 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
                 category: category,
                 onSelectCategory: () {
-                  _selectCategory(
-                      context); // navigating to the meals screen and using an anonymous function to pass the context
+                  _selectCategory(context,
+                      category); // navigating to the meals screen and using an anonymous function to pass the context
                 }),
         ],
       ),
